3922bf3002dfbc29612782e24c7355a4e3d741ef
Baz00k <gustav.buzuk@gmail.com>
2021-04-07 19:07:05 +0200

    page fixes and optimalisation

---
+++ js/plugins.prod.js
@@ -0,0 +1,1 @@
+"use strict";!function(){for(var e,o=function(){},r=["assert","clear","count","debug","dir","dirxml","error","exception","group","groupCollapsed","groupEnd","info","log","markTimeline","profile","profileEnd","table","time","timeEnd","timeStamp","trace","warn"],n=r.length,i=window.console=window.console||{};n--;)i[e=r[n]]||(i[e]=o)}();
